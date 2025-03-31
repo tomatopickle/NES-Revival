7180033a8767958410af1507e8cc105c281d20a5
Abaan Shanid <abaanashanid@gmail.com>
2025-03-31 23:46:46 +0530

    Fix: home page not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 // App.jsx
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Game from './Game';
+import React from "react";
+import { BrowserRouter as HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Game from "./Game";
 
 const App = () => {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/game" element={<Game />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 
